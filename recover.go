9029992336fdceb530a7be4a9495227114930859
devlemonking <lkdevteam@hotmail.com>
2023-12-06 03:57:34 +0330

    Add recover function to handle panics

---
+++ recover.go
@@ -0,0 +1,20 @@
+package errors
+
+// Recover recovers from panic and sets the error pointer to the recovered error.
+//
+// Can be used in conjunction with Must to handle any errors that may occur.
+func Recover(errp *error) {
+	if r := recover(); r != nil {
+		if errp == nil {
+			return
+		}
+
+		err, ok := r.(error)
+		if !ok {
+			// Skipping 3 stack frames: recover.go, panic.go, must.go
+			err = NewSkipf(3, "%v", r)
+		}
+
+		*errp = err
+	}
+}
